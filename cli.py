bc723194b4de0a281243d96782e349cdcb11c0ae
LordA2117 <88305634+LordA2117@users.noreply.github.com>
2024-03-22 08:34:47 +0530

    Added quality of life features to cli

--- cli.py
+++ cli.py
@@ -4,11 +4,18 @@ from pprint import pprint
 
 from web import get_data, get_token
 
+"""This is the CLI for the Academia ripoff. It will provide all the obtainable details of a student in json/dictionary format"""
+
 parser = argparse.ArgumentParser()
 parser.add_argument("--username", type=str, help="SRM netID username")
 
 parser.add_argument("--password", type=str,
                     help="SRM password, leave blank for secure password entry")
+
+parser.add_argument("--output", help="Outputs the data into a file", type=str)
+
+parser.add_argument(
+    "--json", choices=[0, 1], type=int, help="Enter 1 to output it as json")
 args = parser.parse_args()
 username = args.username
 
@@ -26,6 +33,17 @@ token = get_token(username=username, password=password)
 data = get_data(token_dict=token)
 
 pprint(data)
+
+if args.json:
+    import json
+    with open(args.output, mode="w", encoding='utf-8') as file:
+        json.dump(data, file)
+
+else:
+    if args.output:
+        with open(args.output, mode="w", encoding='utf-8') as file:
+            file.write(str(data))
+
 
 # TODO: Account for errors such as 404, 302, 500, etc.
 # TODO: Web Interface and database models.
