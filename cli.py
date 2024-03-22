f136bef24709170e31f54dffe58b7b4738e9f17c
LordA2117 <88305634+LordA2117@users.noreply.github.com>
2024-03-22 08:17:36 +0530

    Added secure password input

--- cli.py
+++ cli.py
@@ -1,4 +1,5 @@
 import argparse
+from getpass import getpass
 from pprint import pprint
 
 from web import get_data, get_token
@@ -6,10 +7,24 @@ from web import get_data, get_token
 parser = argparse.ArgumentParser()
 parser.add_argument("--username", type=str, help="SRM netID username")
 
-parser.add_argument("--password", type=str, help="SRM password")
-
+parser.add_argument("--password", type=str,
+                    help="SRM password, leave blank for secure password entry")
 args = parser.parse_args()
-token = get_token(username=args.username, password=args.password)
+username = args.username
+
+if not username:
+    print("Enter a username and continue")
+    exit()
+
+if not args.password:
+    password = getpass()
+else:
+    password = args.password
+
+
+token = get_token(username=username, password=password)
 data = get_data(token_dict=token)
 
 pprint(data)
+
+# TODO: Account for errors such as 404, 302, 500, etc.
